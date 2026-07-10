eb69809449af91737da958bdd5e8a1e20a8b05ab
Гроза Влад <vladut7373@gmail.com>
2026-07-10 12:43:06 +0300

    Update SettingsTab.kt

--- app/src/main/java/com/wdtt/client/ui/SettingsTab.kt
+++ app/src/main/java/com/wdtt/client/ui/SettingsTab.kt
@@ -10,6 +10,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.clickable
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
